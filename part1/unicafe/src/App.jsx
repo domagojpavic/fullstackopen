bd34cbab42963a616a1c31221a392d8bb03389f5
Domagoj Pavić <dpavic@gmail.com>
2024-02-09 23:51:08 +0100

    part1 unicafe step1 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,12 +1,42 @@
 import { useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGood = () => {
+    return (
+      setGood(good + 1)
+    )
+  }
+  const handleNeutral = () => {
+    return (
+      setNeutral(neutral + 1)
+    )
+  }
+  const handleBad = () => {
+    return (
+      setBad(bad + 1)
+    )
+  }
 
   return (
-    <>
+    <div>
 
-    </>
+    <h1>give feedback</h1>
+    <button onClick={handleGood}>good</button>
+    <button onClick={handleNeutral}>neutral</button>
+    <button onClick={handleBad}>bad</button>
+    
+
+    <h1>statistics</h1>
+    <p>
+      good {good}<br/>
+      neutral {neutral}<br/>
+      bad {bad}
+    </p>
+    </div>
   )
 }
 
